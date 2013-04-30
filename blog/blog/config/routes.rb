3728f5e685f6a9199bd8ace618687a6164b42950
Lucas Trombeta <lucastrombet@gmail.com>
2013-04-30 14:48:37 -0700

    First part done

--- blog/blog/config/routes.rb
+++ blog/blog/config/routes.rb
@@ -1,4 +1,10 @@
 Blog::Application.routes.draw do
+
+  resources :posts
+
+
+  resources :posts
+  root :to => "posts#index"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
