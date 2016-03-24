7a35016dce43da6e3ca46e0406ed0358713e7e13
Steve Blandon <steveblandon@gmail.com>
2016-03-24 10:25:06 -0400

    fixed path issue

--- RailsInstaller/Ruby2.2.0/bin/rails.bat
+++ RailsInstaller/Ruby2.2.0/bin/rails.bat
@@ -1,6 +1,7 @@
 @ECHO OFF
 IF NOT "%~f0" == "~f0" GOTO :WinNT
-@"C:\Users\emachnic\GitRepos\railsinstaller-windows\stage\Ruby2.2.0\bin\ruby.exe" "C:/Users/emachnic/GitRepos/railsinstaller-windows/stage/Ruby2.2.0/bin/tilt" %1 %2 %3 %4 %5 %6 %7 %8 %9
+@"C:\RailsInstaller\Ruby2.2.0\bin\ruby.exe" "C:\RailsInstaller\Ruby2.2.0\bin\rails" %1 %2 %3 %4 %5 %6 %7 %8 %9
 GOTO :EOF
 :WinNT
-@"C:\Users\emachnic\GitRepos\railsinstaller-windows\stage\Ruby2.2.0\bin\ruby.exe" "%~dpn0" %*
+@"C:\RailsInstaller\Ruby2.2.0\bin\ruby.exe" "%~dpn0" %*
+
