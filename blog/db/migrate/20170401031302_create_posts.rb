ad1c2cdd0abedbb6933d51a0e8d36e058a2e7298
vk450class <class450github@gmail.com>
2017-04-01 20:52:54 -0700

    Added blog entry image option, blog entries, show/hide full article.
    Added option to allow showing an image with a blog entry. Created some blog entries (seeds.rb). Added functionality to show the rest of the article ('Read more').

--- blog/db/migrate/20170401031302_create_posts.rb
+++ blog/db/migrate/20170401031302_create_posts.rb
@@ -3,6 +3,7 @@ class CreatePosts < ActiveRecord::Migration[5.0]
     create_table :posts do |t|
       t.string :title
       t.text :content
+      t.string :imageSrc
 
       t.timestamps
     end
--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170330195718) do
+ActiveRecord::Schema.define(version: 20170401031302) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
+    t.string   "imageSrc"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
