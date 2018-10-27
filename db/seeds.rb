3ee1418a324a83cc235bf330f310dfcd9fe1f846
Lizzie <lizzie.sorin@hotmail.fr>
2018-10-27 12:41:13 -0700

    track blog and post scaffold

---
+++ db/seeds.rb
@@ -0,0 +1,63 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+post1 = Post.create(
+	title: "How Google Protected Andy Rubin, the ‘Father of Android",
+	content: "SAN FRANCISCO — Google gave Andy Rubin, the creator of Android mobile software, a hero’s 
+	farewell when he left the company in October 2014. “I want to wish Andy all the best with what’s next,”
+	 Larry Page, Google’s chief executive then, said in a public statement. “With Android he created 
+	 something truly remarkable — with a billion-plus happy users.” What Google did not make public was 
+	 that an employee had accused Mr. Rubin of sexual misconduct. The woman, with whom Mr. Rubin had been 
+	 having an extramarital relationship, said he coerced her into performing oral sex in a hotel room in 
+	 2013, according to two company executives with knowledge of the episode. Google investigated and 
+	 concluded her claim was credible, said the people, who spoke on the condition that they not be named, 
+	 citing confidentiality agreements. Mr. Rubin was notified, they said, and Mr. Page asked for his 
+	 resignation.
+	 Google could have fired Mr. Rubin and paid him little to nothing on the way out. Instead, the company 
+	 handed him a $90 million exit package, paid in installments of about $2 million a month for four years, 
+	 said two people with knowledge of the terms. The last payment is scheduled for next month."
+	) 
+
+post2 = Post.create(
+	title: "A.I. Is Helping Scientists Predict When and Where the Next Big Earthquake Will Be",
+	content: "SAN FRANCISCO — Countless dollars and entire scientific careers have been dedicated to 
+	predicting where and when the next big earthquake will strike. But unlike weather forecasting, which 
+	has significantly improved with the use of better satellites and more powerful mathematical models, 
+	earthquake prediction has been marred by repeated failure.
+	Some of the world’s most destructive earthquakes — China in 2008, Haiti in 2010 and Japan in 2011, 
+	among them — occurred in areas that seismic hazard maps had deemed relatively safe. The last large 
+	earthquake to strike Los Angeles, Northridge in 1994, occurred on a fault that did not appear on 
+	seismic maps.
+	Now, with the help of artificial intelligence, a growing number of scientists say changes in the way 
+	they can analyze massive amounts of seismic data can help them better understand earthquakes, 
+	anticipate how they will behave, and provide quicker and more accurate early warnings.
+
+	“I am actually hopeful for the first time in my career that we will make progress on this problem,” 
+	said 
+	Paul Johnson, a fellow at the Los Alamos National Laboratory who is among those at the forefront of 
+	this research."
+	)
+
+post3 = Post.create(
+	title: "Facebook Removes Iranian Network That Was Spreading Disinformation",
+	content: "SAN FRANCISCO — Last year, when Facebook disclosed a sweeping, coordinated disinformation 
+	campaign carried out by Russian agents, the company presented the first evidence of a new kind of 
+	information warfare.
+	Now mischief makers in other countries appear to be following the Russian playbook.
+	Facebook said on Friday that it had identified and removed a new influence network that had originated 
+	in Iran. It was the second time this year that a disinformation effort targeting people in the United 
+	States and Britain had been tied to Iranians.
+	The company took down more than 82 pages, groups and accounts, including 30 Facebook pages, 33 
+	Instagram accounts and three Facebook groups, it said. About one million users in the United States 
+	and Britain followed those accounts.
+	The latest disinformation campaign, which Facebook said was discovered last week, was much smaller 
+	than the Russia-linked information operations before and after the 2016 presidential election. Back 
+	then, millions of Facebook and Instagram users viewed divisive ads and disinformation that investigators 
+	say were created with the aim of widening political and social divisions among Americans."
+	)  
+
