91111fd48bc6513bf7420ba5c71d7adb809ea61a
Lizzie <lizzie.sorin@hotmail.fr>
2018-10-28 08:32:27 -0700

    build signup page

--- blog/blog/app/controllers/users_controller.rb
+++ blog/blog/app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+
+	def new
+	end
+
+	def create
+		user = User.new(
+			name: params[:email],
+			password: params[:password],
+			password_confirmation: params[:password_confirmation]
+		)
+		# require bootstrap gem
+		if user.save
+			session[:user_id] = user.user_id 
+			flash[:success] = "You signed up successfully!"
+			redirect_to "/posts"
+		else
+			flash[:warning] = "Invalid Email or Password"
+			redirect_to "/signup"
+		end
+	end
 end
--- blog/blog/config/routes.rb
+++ blog/blog/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :posts
   root :to => "posts#index"
+  get "/signup" => "users#new"
+  post "/users" => "users#create"
 end
