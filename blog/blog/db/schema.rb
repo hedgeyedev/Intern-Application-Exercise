283b10a924f01dc381962d1f4b1375211aac21f1
Lizzie <lizzie.sorin@hotmail.fr>
2018-10-28 08:16:35 -0700

    has secure password

--- blog/blog/db/schema.rb
+++ blog/blog/db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181027172523) do
+ActiveRecord::Schema.define(version: 20181028151119) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
