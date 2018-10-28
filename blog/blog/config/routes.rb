4297523dc0ba505f5d9a52cafa86845dbf138193
Lizzie <lizzie.sorin@hotmail.fr>
2018-10-28 08:34:27 -0700

    add session routes

--- blog/blog/config/routes.rb
+++ blog/blog/config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   root :to => "posts#index"
   get "/signup" => "users#new"
   post "/users" => "users#create"
+  get "/login" => "sessions#new"
+  post "/login" => "sessions#create"
+  get "/logout" => "sessions#destroy"
 end
