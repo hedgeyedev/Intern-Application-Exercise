0d709c017bd5d3b62f239518730f3d5cb7a512f0
Lizzie <lizzie.sorin@hotmail.fr>
2018-10-29 19:24:31 -0700

    adjust spaces

--- blog/blog/app/assets/javascripts/application.js
+++ blog/blog/app/assets/javascripts/application.js
@@ -21,22 +21,18 @@ $(function() {
 });
 // attach the listeners of the element that display on the document
 function attachListeners() {
-    $(".thumb-up").on("click", disableButton);        
+    $(".thumb-up").on("click", disableButtonLike);
+    $(".thumb-down").on("click", disableButtonDislike);         
 }
 
-function disableButton() {
-	
+function disableButtonLike() {
 	$('#form_to_disable').one('submit', function() {
-		
     	$(this).find('button[type="submit"]').attr('disabled','disabled');
 	});
-	// var buttonLike = document.getElementsByClassName("thumb-up");
-	// buttonLike.type = "";
-	// $('.like_form').append(`
-	// 	<button type="text" class="thumb-up">
-	//     	<i class="far fa-thumbs-up thumbs"></i> <%=@post.likes%>
-	// 	</button>
-	// `)
-	// $('.thumb-up').addClass('hide');
-	// $('.like_form').append('<i class=`far fa-thumbs-up thumbs`></i> <%=@post.likes%> hello')
+}
+
+function disableButtonDislike() {
+	$('#form_to_disable2').one('submit', function() {
+    	$(this).find('button[type="submit"]').attr('disabled','disabled');
+	});
 }
