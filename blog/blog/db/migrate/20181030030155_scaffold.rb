4972ba5b33168e98cac64fd81d47f8685a9ae0ba
Lizzie <lizzie.sorin@hotmail.fr>
2018-10-29 20:04:01 -0700

    likes scaffold

---
+++ blog/blog/db/migrate/20181030030155_scaffold.rb
@@ -0,0 +1,4 @@
+class Scaffold < ActiveRecord::Migration[5.1]
+  def change
+  end
+end
--- blog/blog/db/schema.rb
+++ blog/blog/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181029155334) do
+ActiveRecord::Schema.define(version: 20181030030155) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
