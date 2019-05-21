f29004e5c41f56738916f81436405f8c00d54df2
Brandon <bagastelo@cpp.edu>
2019-05-21 21:40:01 +0000

    Add tests for posts controller

--- blog/blog/test/controllers/posts_controller_test.rb
+++ blog/blog/test/controllers/posts_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class PostsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @post = posts(:one)
+    @auth = { Authorization: ActionController::HttpAuthentication::Basic.encode_credentials('admin', 'supersecret') }
   end
 
   test "should get index" do
@@ -11,16 +12,29 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get new" do
-    get new_post_url
+    get new_post_url, headers: @auth
     assert_response :success
+  end
+  
+  test "should not get new if unauthorized" do
+    get new_post_url
+    assert_response :unauthorized
   end
 
   test "should create post" do
     assert_difference('Post.count') do
-      post posts_url, params: { post: { content: @post.content, title: @post.title } }
+      post posts_url, params: { post: { content: @post.content, title: @post.title } }, headers: @auth
     end
 
     assert_redirected_to post_url(Post.last)
+  end
+  
+  test "should not create post if unauthorized" do
+    assert_difference('Post.count', 0) do
+      post posts_url, params: { post: { content: @post.content, title: @post.title } }
+    end
+    
+    assert_response :unauthorized
   end
 
   test "should show post" do
@@ -29,20 +43,38 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get edit" do
-    get edit_post_url(@post)
+    get edit_post_url(@post), headers: @auth
     assert_response :success
+  end
+  
+  test "should not get edit if unauthorized" do
+    get edit_post_url(@post)
+    assert_response :unauthorized
   end
 
   test "should update post" do
-    patch post_url(@post), params: { post: { content: @post.content, title: @post.title } }
+    patch post_url(@post), params: { post: { content: @post.content, title: @post.title } }, headers: @auth
     assert_redirected_to post_url(@post)
+  end
+  
+  test "should not update post if unauthorized" do
+    patch post_url(@post), params: { post: { content: @post.content, title: @post.title } }
+    assert_response :unauthorized
   end
 
   test "should destroy post" do
     assert_difference('Post.count', -1) do
-      delete post_url(@post)
+      delete post_url(@post), headers: @auth
     end
 
     assert_redirected_to posts_url
+  end
+  
+  test "should not destroy postif unauthorized" do
+    assert_difference('Post.count', 0) do
+      delete post_url(@post)
+    end
+    
+    assert_response :unauthorized
   end
 end
