eb84bfb6cb30edb94466b2a5c71f4db55f2144f3
Christopher Chang <chiggiechang@gmail.com>
2022-01-29 19:03:05 -0800

    Avoid warning with spell check, at least with Rails 6.1.4.4
    To achieve this, I repeated the basic steps to make the "butt ugly"
    blog. That is, I created a rails project in the blog directory again,
    created a scaffold (and did a migration) again, and set up the routes
    again.

--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_28_230727) do
+ActiveRecord::Schema.define(version: 2022_01_30_030048) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
