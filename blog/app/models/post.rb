fb65a045b3f235fba8dc66213ab86b98e9f75a4c
Christopher Chang <chiggiechang@gmail.com>
2022-01-29 22:26:38 -0800

    Show post previews rather than a table of all posts

--- blog/app/models/post.rb
+++ blog/app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ApplicationRecord
+  MAX_OPENING_CHARS = 400.freeze
+
+  # Returns the initial part of a blog post's content
+  def opening
+    content.size > MAX_OPENING_CHARS ? content[...MAX_OPENING_CHARS] + '...' : content
+  end
 end
