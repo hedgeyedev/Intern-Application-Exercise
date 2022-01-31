ba3741a0a65dfb3a3b8f396b769778f5b668df2d
Christopher Chang <chiggiechang@gmail.com>
2022-01-30 18:57:56 -0800

    Use Action Text for rich text in blog posts, display entire posts

--- blog/app/models/post.rb
+++ blog/app/models/post.rb
@@ -1,8 +1,3 @@
 class Post < ApplicationRecord
-  MAX_OPENING_CHARS = 400.freeze
-
-  # Returns the initial part of a blog post's content
-  def opening
-    content.size > MAX_OPENING_CHARS ? content[...MAX_OPENING_CHARS] + '...' : content
-  end
+  has_rich_text :content
 end
