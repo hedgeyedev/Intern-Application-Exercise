391e79e4e22a0c0cdebaa79db0587ccbd6a5349a
Christopher Chang <chiggiechang@gmail.com>
2022-01-30 21:53:27 -0800

    Implement basic commenting, with Bootstrap styling

---
+++ blog/app/controllers/comments_controller.rb
@@ -0,0 +1,13 @@
+class CommentsController < ApplicationController
+  before_action :set_post
+
+  def create
+    @post.comments.create! params.required(:comment).permit(:content)
+    redirect_to @post
+  end
+
+  private
+    def set_post
+      @post = Post.find(params[:post_id])
+    end
+end
--- blog/app/models/post.rb
+++ blog/app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ApplicationRecord
   has_rich_text :content
+  has_many :comments
 end
