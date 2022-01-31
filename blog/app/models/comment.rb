b08d5bc067ff5f63cb84bf786dfac9230e036ea8
Christopher Chang <chiggiechang@gmail.com>
2022-01-30 22:11:45 -0800

    Use Action Text for rich text in comments

--- blog/app/models/comment.rb
+++ blog/app/models/comment.rb
@@ -1,3 +1,4 @@
 class Comment < ApplicationRecord
   belongs_to :post
+  has_rich_text :content
 end
--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_31_053121) do
+ActiveRecord::Schema.define(version: 2022_01_31_060851) do
 
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
@@ -52,7 +52,6 @@ ActiveRecord::Schema.define(version: 2022_01_31_053121) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "post_id", null: false
-    t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
