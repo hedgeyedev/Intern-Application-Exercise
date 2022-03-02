b33b7adc4fc455ce122e33369376e76fe83a5627
Lucas O'Donnell <lucas.odonnelll@gmail.com>
2022-03-01 21:54:01 -0800

    Blog post has preview before going to show page. Changed some styling

--- blog/blog/app/controllers/posts_controller.rb
+++ blog/blog/app/controllers/posts_controller.rb
@@ -44,6 +44,6 @@ class PostsController < ApplicationController
 
   private
   def post_params
-    params.require(:post).permit(:title, :post)
+    params.require(:post).permit(:title, :post, :content)
   end
 end
--- blog/blog/app/models/post.rb
+++ blog/blog/app/models/post.rb
@@ -1,6 +1,17 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
-  has_rich_text :post
+  has_rich_text :content
   validates :title, :post, presence: true, allow_blank: false
+
+  before_validation :plain_post
+
+  def plain_post
+    text = self.rich_text_content.to_plain_text
+    self.post = text
+  end
+
+  def post_preview
+    self.post.split(" ")[0..100].join(" ")
+  end
 end
