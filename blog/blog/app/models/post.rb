d683acf512859187ceae45b32860a48720828539
Lucas O'Donnell <lucas.odonnelll@gmail.com>
2022-03-01 19:28:19 -0800

    Used action-text to improve the formatting and look of blog posts

--- blog/blog/app/models/post.rb
+++ blog/blog/app/models/post.rb
@@ -1,10 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
+  has_rich_text :post
   validates :title, :post, presence: true, allow_blank: false
-
-  def post_introduction
-    self.post 
-    post.split(" ")[0..10].join(" ")
-  end
 end
